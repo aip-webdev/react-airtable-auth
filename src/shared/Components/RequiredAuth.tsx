0654ab37f86479ef4ba2dd8d9a48dfed781523a4
Alexey Petrenko <39764091+aip-webdev@users.noreply.github.com>
2021-11-30 13:11:10 +0600

    Add files via upload

--- src/shared/Components/RequiredAuth.tsx
+++ src/shared/Components/RequiredAuth.tsx
@@ -1,14 +1,15 @@
 import {useNavigate} from "react-router-dom";
 import {useAppStore} from "../../hooks/useAppStore";
-import React from "react";
+import React, {useEffect} from "react";
 
 export function RequiredAuth({ children }: { children: JSX.Element }) {
     const [state] = useAppStore();
     const navigate = useNavigate();
-    if (!state.isAuth) {
-        return (
-            navigate("/signin")
-        );
-    }
+    useEffect(() => {
+        if (!state.isAuth) {
+             navigate("/signin")
+        }
+    }, [])
+
     return children;
 }
